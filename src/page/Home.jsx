1a2f069c67ca87a7e9e95bb18335e2f2b2d8666c
killer0210 <saihants1@gmail.com>
2026-03-05 18:24:58 +0800

    price section product

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,4 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import hero_image from '../assets/png/preview.png'
 import {
   book_10,
   setguul_6,
@@ -28,7 +34,98 @@ import {
   alibom_5,
 } from "../assets";
 
+const products = [
+  {
+    id: 1,
+    name: "Ном",
+    image: book_10,
+  },
+
+  {
+    id: 2,
+    name: "Тайлан",
+    image: tailan_4,
+  },
+
+  {
+    id: 3,
+    name: "Сэтгүүл",
+    image:setguul_6,
+  },
+  {
+    id: 4,
+    name: "Нэрийн хуудас",
+    image: neriinhuudas_5,
+  },
+  {
+    id: 5,
+    name: "Шошго",
+    image: book_25,
+  },
+  {
+    id: 6,
+    name: "Танилцуулга",
+    image: taniltsuulga_14,
+  },
+  {
+    id: 7,
+    name: "Урилга,Мэндчилгээ",
+    image: tailan_6,
+  },
+  {
+    id: 8,
+    name: "Календарь",
+    image: book_12,
+  },
+
+  {
+    id: 9,
+    name: "Альбом панобүүк",
+    image: book_18,
+  },
+  {
+    id: 10,
+    name: "Дипломын хавтас",
+    image: alibom_3,
+  },
+  {
+    id: 11,
+    name: "Дэвтэр",
+    image: setguul_3,
+  },
+  {
+    id: 12,
+    name: "Маягт",
+    image: neriinhuudas_5,
+  },
+  {
+    id: 13,
+    name: "Нэрийн хуудас",
+    image: neriinhuudas_5,
+  },
+  {
+    id: 14,
+    name: "Албан бланк,дугтуй",
+    image: taniltsuulga_8,
+  },
+  {
+    id: 15,
+    name: "Меню",
+    image: setguul_6,
+  },
+  {
+    id: 16,
+    name: "Тасалбар,талон",
+    image: taniltsuulga_8,
+  },
+];
+
+
+
 const Home = () => {
+
+    const [isHovered, setIsHovered] = useState(false);
+    
   return (
     <>
       <section className="bg-gradient-primary text-white text-center">
@@ -36,7 +133,7 @@ const Home = () => {
           <div className="grid grid-cols-6 grid-rows-6  h-[600px]">
             <div className="col-start-1 col-end-4 row-start-1 row-end-6 rounded-tl-3xl rounded-bl-3xl overflow-hidden relative">
               <div
-                className="absolute inset-0 rounded-tl-3xl rounded-bl-3xl hero-overlay-glass"
+                className="absolute inset-0 rounded-tl-3xl rounded-bl-3xl bg-white/30 backdrop-blur-md"
                 aria-hidden="true"
               ></div>
               <div className="relative z-10 px-5 py-5 h-full my-auto text-start space-y-6 flex flex-col justify-center">
@@ -53,9 +150,9 @@ const Home = () => {
                   <button className="bg-[var(--primary-color,#00AEEF)] hover:bg-[var(--tertiary-color,#0018F0)] text-[var(--text-inverse,#fff)] font-semibold py-3 px-6 rounded-lg shadow transition-colors duration-150">
                     Үнийн санал авах
                   </button>
-                  <button className="bg-[var(--secondary-color,#0064F0)] hover:bg-[var(--primary-color,#00AEEF)] text-[var(--text-inverse,#fff)] font-semibold py-3 px-6 rounded-lg shadow transition-colors duration-150">
+                  {/* <button className="bg-[var(--secondary-color,#0064F0)] hover:bg-[var(--primary-color,#00AEEF)] text-[var(--text-inverse,#fff)] font-semibold py-3 px-6 rounded-lg shadow transition-colors duration-150">
                     Бүтээгдэхүүн
-                  </button>
+                  </button> */}
                 </div>
               </div>
             </div>
@@ -73,10 +170,12 @@ const Home = () => {
                 <p className="text-6xl font-semibold">30</p>
                 <span className="text-md font-medium">Мянган ном</span>
               </div>
+              <div className="tag-right-top bg-white/30 backdrop-blur-md"></div>
             </div>
-            <div
-              className=" col-start-4 col-end-7 row-start-1 row-end-7 rounded-tr-3xl rounded-bl-3xl rounded-br-3xl hero-overlay-glass"
-            ></div>
+            <div className=" col-start-4 col-end-7 row-start-1 row-end-7 rounded-tr-3xl rounded-bl-3xl rounded-br-3xl bg-white/30 backdrop-blur-md">
+            <div >
+            <img src={hero_image} alt="" className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out" /></div>
+            </div>
           </div>
         </div>
       </section>
@@ -87,97 +186,102 @@ const Home = () => {
               ОНЦЛОХ <br /> БҮТЭЭГДЭХҮҮНҮҮД
             </h2>
           </div>
-          <div className="col-start-3 col-end-4 row-start-1 row-end-2 bg-gray-200 rounded-lg overflow-hidden">
-            <img src={book_10} alt="" className="w-full h-full object-cover" />
+          <div className="col-start-3 col-end-4 row-start-1 row-end-2 bg-gray-200 rounded-lg overflow-hidden relative">
+            <img
+              src={book_10}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-4 col-end-5 row-start-1 row-end-3 bg-green-500 rounded-lg overflow-hidden">
             <img
               src={setguul_6}
               alt=""
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
             />
           </div>
           <div className="col-start-3 col-end-4 row-start-2 row-end-4 bg-red-500 rounded-lg overflow-hidden">
-            <img src={tailan_4} alt="" className="w-full h-full object-cover" />
+            <img
+              src={tailan_4}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-4 col-end-5 row-start-3 row-end-5 bg-yellow-500 rounded-lg overflow-hidden">
-            <img src={book_12} alt="" className="w-full h-full object-cover" />
+            <img
+              src={book_12}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-3 col-end-4 row-start-4 row-end-5 bg-purple-500 rounded-lg overflow-hidden">
-            <img src={book_18} alt="" className="w-full h-full object-cover" />
+            <img
+              src={book_18}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-2 col-end-3 row-start-2 row-end-3 bg-pink-500 rounded-lg overflow-hidden">
-            <img src={tailan_6} alt="" className="w-full h-full object-cover" />
+            <img
+              src={tailan_6}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-2 col-end-3 row-start-3 row-end-5 bg-blue-500 rounded-lg overflow-hidden">
-            <img src={book_19} alt="" className="w-full h-full object-cover" />
+            <img
+              src={book_19}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-1 col-end-2 row-start-4 row-end-5 bg-gray-500 rounded-lg overflow-hidden">
-            <img src={book_20} alt="" className="w-full h-full object-cover" />
+            <img
+              src={book_20}
+              alt=""
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
+            />
           </div>
           <div className="col-start-1 col-end-2 row-start-2 row-end-4 bg-gray-500 rounded-lg overflow-hidden">
             <img
               src={setguul_7}
               alt=""
-              className="w-full h-full object-cover"
+              className="w-full h-full object-cover hover:scale-105 transition-all duration-300 ease-in-out"
             />
           </div>
         </div>
       </section>
 
       <section>
-        <div className="text-center mb-12 bg-[#0064F0] text-white">
-          <div className="max-w-[1200px] mx-auto flex flex-col items-center justify-center gap-4 py-12 px-5">
-            <div className="flex items-center justify-between w-full">
-              <h2 className="text-4xl font-semibold">Бүтээгдэхүүн</h2>
+        <div className="max-w-[1200px] mx-auto px-4">
+          <div className="bg-[#0064F0] text-white py-12 px-5 rounded-xl">
+            <div className="flex items-center justify-between w-full mb-6">
+              <h2 className="text-4xl font-semibold ">Бүтээгдэхүүн</h2>
               <a
                 href="#"
-                className="text-white text-sm font-medium hover:underline"
+                className="text-white text-sm font-medium hover:border-b-2 border-white"
               >
-                Бүгд
+                <span className="mr-2">Бүгд</span>
+                <i className="fa-solid fa-arrow-right"></i>
               </a>
             </div>
-            <div className="grid grid-cols-4 gap-4 h-auto  ">
-              <div className="space-y-2 text-center flex flex-col items-center justify-center">
-                <div className="rounded-lg overflow-hidden">
-                  <img
-                    src={book_12}
-                    alt="Ном"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <h3 className="text-2xl font-semibold">Ном</h3>
-              </div>
-              <div className="space-y-2 text-center flex flex-col items-center justify-center">
-                <div className="rounded-lg overflow-hidden">
-                  <img
-                    src={tailan_4}
-                    alt="Ном"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <h3 className="text-2xl font-semibold">Тайлан</h3>
-              </div>
-              <div className="space-y-2 text-center flex flex-col items-center justify-center">
-                <div className="rounded-lg overflow-hidden">
-                  <img
-                    src={alibom_3}
-                    alt="Ном"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <h3 className="text-2xl font-semibold">Сэтгүүл</h3>
-              </div>
-              <div className="space-y-2 text-center flex flex-col items-center justify-center">
-                <div className="rounded-lg overflow-hidden">
-                  <img
-                    src={taniltsuulga_14}
-                    alt="Ном"
-                    className="w-full h-full object-cover"
-                  />
-                </div>
-                <h3 className="text-2xl font-semibold">Танилцуулга</h3>
-              </div>
+            <div className="w-full overflow-x-auto scrollbar-hide">
+              <Swiper slidesPerView={3.5} spaceBetween={16} className="relative rounded-lg overflow-hidden h-[200px]">
+              <div className="pointer-events-none absolute left-0 top-0 h-full w-12 bg-gradient-to-r from-black to-transparent z-10 " />
+              <div className="pointer-events-none absolute right-0 top-0 h-full w-12 bg-gradient-to-l from-black to-transparent z-10" />
+                {products.map((product) => (
+                  <SwiperSlide key={product.id} >
+                    <div className=" rounded-lg overflow-hidden relative shrink-0 cursor-pointer group h-full">
+                      <img src={product.image} alt={product.name} className="w-full h-full object-cover group-hover:scale-105 transition-all duration-300" />
+                    </div>
+                    <div className="absolute bottom-0 left-0 w-full h-20 bg-gradient-to-t from-black/80 to-transparent z-10 flex items-end justify-center rounded-b-lg">
+                      <h3 className="text-white text-xl font-large p-4">
+                        {product.name}
+                      </h3>
+                    </div>
+                  </SwiperSlide>
+                ))}
+              </Swiper>
             </div>
           </div>
         </div>
@@ -188,8 +292,9 @@ const Home = () => {
         <div className="max-w-[1200px] mx-auto px-5">
           <div className="flex items-center justify-between w-full mb-6 ">
             <h2 className=" text-4xl font-semibold">Ашигладаг технологи</h2>
-            <a href="#" className="text- text-sm font-medium hover:underline">
-              Бүгд
+            <a href="#" className="text- text-sm font-medium hover:border-b-2 border-black">
+              <span className="mr-2">Бүгд</span>
+              <i className="fa-solid fa-arrow-right"></i>
             </a>
           </div>
           <div className="grid grid-cols-4 grid-rows-3 gap-[15px] h-[600px] ">
@@ -269,20 +374,25 @@ const Home = () => {
       </section>
 
       {/* PRICING CTA */}
-      <section className="bg-[var(--secondary-color,#0064F0)] text-white">
-        <div className="max-w-[1200px] mx-auto px-5 flex items-center justify-between gap-8 py-12">
-          <div className="w-1/2">
+      <section className="w-full">
+        <div className="max-w-[1200px] mx-auto px-5  ">
+            <div className="flex items-center justify-between w-full gap-8 bg-[var(--secondary-color,#0064F0)] text-white rounded-xl overflow-hidden py-12 px-6">
+            <div className="w-1/2">
             <h2 className="text-4xl font-semibold">Үнийн санал &</h2>
             <p className="text-lg mt-2">
               Загвар,хэмжээ, материал, тоо ширхэгээ оруулаад шууд үнэ санал авах
             </p>
             <button className="px-7 py-3.5 rounded-lg border-none cursor-pointer mt-5 bg-gray-900 text-white font-medium hover:opacity-90">
-              Үнэ санал авах
+              
+              
+              <span >Үнэ санал авах</span>
+              
             </button>
           </div>
           <div className="w-1/2 overflow-hidden ">
             <img src={book_25} alt="" className="w-full h-full object-cover" />
           </div>
+            </div>
         </div>
       </section>
 
@@ -367,13 +477,13 @@ const Home = () => {
       {/* WHAT WE DO */}
       <section className="py-12">
         <div className="max-w-[1200px] mx-auto px-5 grid grid-cols-1 md:grid-cols-3 gap-8">
+        <div className="bg-white p-8 rounded-2xl">
+            <h3 className="text-lg font-semibold mb-2">🎨 Дизайн</h3>
+            <p>Мэргэжлийн дизайн үйлчилгээ</p>
+          </div>
           <div className="bg-white p-8 rounded-2xl">
             <h3 className="text-lg font-semibold mb-2">📄 Хэвлэл</h3>
             <p>Өндөр чанартай хэвлэл үйлдвэрлэл</p>
-          </div>
-          <div className="bg-white p-8 rounded-2xl">
-            <h3 className="text-lg font-semibold mb-2">🎨 Дизайн</h3>
-            <p>Мэргэжлийн дизайн үйлчилгээ</p>
           </div>
           <div className="bg-white p-8 rounded-2xl">
             <h3 className="text-lg font-semibold mb-2">🚚 Хүргэлт</h3>
